dfbaa748e4fb7d1255c55236d3e7ed7115f495e2
Bramigk <victor_bramigk@hotmail.com>
2014-10-22 07:17:14 -0200

    Started TeamAction conversion function

--- prototypes/minimax/inc/action.h
+++ prototypes/minimax/inc/action.h
@@ -2,6 +2,7 @@
 #define MINIMAX_ACTION_H
 
 #include <memory>
+#include "discrete.pb.h"
 
 enum Player { MIN, MAX };
 
@@ -11,7 +12,7 @@ class Action {
 public:
   Action(int r_id) : robot_id(r_id) {}
 
-  int getId() { return robot_id; }
+  int getId() const { return robot_id; }
 
   virtual float getTime() const { return 0; }
 
@@ -19,10 +20,12 @@ public:
 
   enum ActionType { MOVE, PASS, KICK };
   virtual ActionType type() const = 0;
+
+  virtual void discreteAction(roboime::Action*) const = 0;
 };
 
 class Move : public Action {
-  Vector nextPosition;
+  Vector next_position;
   static constexpr float MAX_SPEED = 4; // meters per second
   float time;
 
@@ -35,7 +38,20 @@ public:
 
   ActionType type() const { return MOVE; }
 
-  Vector pos() { return nextPosition; }
+  Vector pos() { return next_position; }
+
+  void discreteAction(roboime::Action* a) const {
+    Vector ux(1,0), uy(0,1);
+
+    a->set_robot_id(getId());
+    a->set_type(roboime::Action::MOVE);
+
+    roboime::Action::Move *move = new roboime::Action::Move();
+    move->set_x(ux * next_position);
+    move->set_y(uy * next_position);
+
+    a->set_allocated_move(move);
+  }
 };
 
 class Pass : public Action {
@@ -49,6 +65,16 @@ public:
   ActionType type() const { return PASS; }
 
   int getId() { return rcv_id; }
+
+  void discreteAction(roboime::Action* a) const {
+    a->set_robot_id(Action::getId());
+    a->set_type(roboime::Action::PASS);
+
+    roboime::Action::Pass *pass = new roboime::Action::Pass();
+    pass->set_robot_id(rcv_id);
+
+    a->set_allocated_pass(pass);
+  }
 };
 
 class Kick : public Action {
@@ -61,6 +87,26 @@ public:
   void apply(Player, Board &) const;
 
   ActionType type() const { return KICK; }
+
+  Vector point2Kick() const {
+    Vector v;
+    // TODO: get some kick point
+    return v;
+  }
+
+  void discreteAction(roboime::Action* a) const {
+    Vector ux(1,0), uy(0,1);
+
+    a->set_robot_id(getId());
+    a->set_type(roboime::Action::KICK);
+
+    roboime::Action::Kick *kick = new roboime::Action::Kick();
+    Vector point_to_kick = point2Kick();
+    kick->set_x(ux * point_to_kick);
+    kick->set_y(uy * point_to_kick);
+
+    a->set_allocated_kick(kick);
+  }
 };
 
 typedef std::vector<std::shared_ptr<Action>> TeamAction;
--- prototypes/minimax/src/action.cpp
+++ prototypes/minimax/src/action.cpp
@@ -5,19 +5,20 @@
 #include "body.h"
 #include "action.h"
 #include "minimax.h"
+#include "discrete.pb.h"
 
 Move::Move(const class Robot &robot) : Action(robot.getId()) {
   Vector position = robot.getLastPlanedPos();
   // srand(time(NULL));//XXX: ????
   if (rand() % 2 == 1) {
     // Move with uniforme distribution
-    nextPosition = robot.getURandPos();
+    next_position = robot.getURandPos();
   } else {
     // Move with normal distribution
-    nextPosition = robot.getNRandPos();
+    next_position = robot.getNRandPos();
   }
   // Compute minimum time
-  time = robot.getDist(nextPosition) / MAX_SPEED;
+  time = robot.getDist(next_position) / MAX_SPEED;
 }
 
 float Move::getTime() const { return time; }
@@ -27,3 +28,12 @@ void Move::apply(Player, Board &b) const {}
 void Pass::apply(Player, Board &b) const {}
 
 void Kick::apply(Player, Board &b) const {}
+
+roboime::Command convert(TeamAction team_action){
+  roboime::Command cmd;
+
+  for(auto& action: team_action)
+    action->discreteAction(cmd.add_action());
+
+  return cmd;
+}
